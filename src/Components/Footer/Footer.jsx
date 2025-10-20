2675cf60f9ba8d7986c164cef3f1d2fc0eaf9b3c
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-20 13:43:29 +0300

    Build Project Skeleton

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Footer</h4>
+            </div>
+        )
+    }
+}
